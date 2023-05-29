05a63da230be8c83a8ab870fe98c6912b85f5a7f
Javier Carrillo <javiercarrillo526@hotmail.com>
2023-05-29 01:00:40 -0300

    serverSocket first commit

---
+++ backend/src/app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const cors = require('cors');
+const dbConnection = require('./config/db.js');
+
+const {API_VERSION, API_NAME} = process.env;
+
+const app = express();
+
+const http = require('http');
+const httpServer = http.createServer(app);
+
+const io = require('socket.io')(httpServer, {
+    cors: {
+        origin: 'http://localhost:4200'
+    }
+});
+
+
+// * Import Routes
+
+app.use(express.urlencoded({extended: true}));        
+app.use(express.json());                    // * Los datos enviados en una solicitud, los recupero en formato Json
+app.use(express.static('src/uploads'));     // * Para poder utilizar/visualizar los archivos que se van a almacenar en uploads
+app.use(cors());
+
+
+app.use((req, res, next) => {
+    req.io = io;
+    req.con = dbConnection;
+    next();
+});
+
+// * Export Routes
+
+// * Escucho los eventos de socket
+
+io.on('connect', (socket) =>{
+    socket.on('disconnect', () => {
+        console.log('Disconnect User');
+    })
+});
+
+module.exports = httpServer;        // * httpServer contiene la información de socket.io
---
+++ backend/src/config/db.js
@@ -0,0 +1,16 @@
+// * Database Connection
+
+
+const mysql = require('mysql');
+require('dotenv').config();
+
+const {DB_HOST,DB_USER, DB_PASSWORD, DB_NAME} =process.env;
+
+const connection = mysql.createConnection({
+    host: DB_HOST,
+    database:DB_NAME,
+    user: DB_USER,
+    password: DB_PASSWORD,
+});
+
+module.exports = connection;
